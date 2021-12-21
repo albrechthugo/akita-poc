c794d879c0875bfa8e6e3720439985405d421ab2
Hugo <albrechthugo4@gmail.com>
2021-12-21 20:43:47 -0300

    chore(deps): include @datorama/akita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools'
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
