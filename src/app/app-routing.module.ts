f324668a61a55cc7de7c30bdc49c04cbf92dc67a
Hugo <albrechthugo4@gmail.com>
2021-12-21 21:01:30 -0300

    wip(employees): create employees module and use lazy loading to render they routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'employees' },
+  { path: 'employees', loadChildren: () => import('./employees/employees.module').then((m) => m.EmployeesModule) }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
